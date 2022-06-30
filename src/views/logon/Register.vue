6ec7801497a7a0b4df19918ffdd557a16fb98aa6
dmidence <40371373+dmidence@users.noreply.github.com>
2022-06-29 21:00:18 -0600

    moving fetch to signin

--- src/views/logon/Register.vue
+++ src/views/logon/Register.vue
@@ -2,75 +2,109 @@
   <Header :logged="false" />
   <div class="main-container logon-bg">
     <div class="logon-container text-center">
-        <i class="fa-solid fa-graduation-cap fa-4x brand-max-widt-icon"></i>
-        <h1 class="my-2">Registro</h1>
-        <hr class="w-100">
+      <i class="fa-solid fa-graduation-cap fa-4x brand-max-widt-icon"></i>
+      <h1 class="my-2">Registro</h1>
+      <hr class="w-100" />
       <span class="vertical-group w-100 my-1">
         <label for="username">Nombre</label>
-        <InputText id="username" placeholder="Ej: Ariel" type="text" class="w-100 p-inputtext-sm" v-model="inputName"/>
+        <InputText
+          id="username"
+          placeholder="Ej: Ariel"
+          type="text"
+          class="w-100 p-inputtext-sm"
+          v-model="inputName"
+        />
       </span>
       <span class="vertical-group w-100 my-1">
         <label for="userlastname">Apellido</label>
-        <InputText id="userlastname" placeholder="Ej: Flores" type="text" class="w-100" v-model="inputLastname" />
+        <InputText
+          id="userlastname"
+          placeholder="Ej: Flores"
+          type="text"
+          class="w-100"
+          v-model="inputLastname"
+        />
       </span>
       <span class="vertical-group w-100 my-1">
         <label for="useremail">Correo</label>
-        <InputText id="useremail" placeholder="Ej: ejemplo@unah.hn " type="text" class="w-100" v-model="inputEmail" />
+        <InputText
+          id="useremail"
+          placeholder="Ej: ejemplo@unah.hn "
+          type="text"
+          class="w-100"
+          v-model="inputEmail"
+        />
       </span>
       <span class="vertical-group w-100 my-1">
         <label for="userpassword">Contraseña</label>
-        <InputText id="userpassword" type="password" class="w-100" v-model="inputPassword" />
+        <InputText
+          id="userpassword"
+          type="password"
+          class="w-100"
+          v-model="inputPassword"
+        />
       </span>
       <span class="vertical-group w-100 my-1">
         <label for="userpasswordv">Confirmar Contraseña</label>
-        <InputText id="userpasswordv" type="password" class="w-100" v-model="inputPasswordv" />
+        <InputText
+          id="userpasswordv"
+          type="password"
+          class="w-100"
+          v-model="inputPasswordv"
+        />
       </span>
       <span class="vertical-group w-100 my-1">
         <Button @click="register()" label="Registrar" class="w-100" />
       </span>
       <span class="vertical-group centered-group w-100 my-1 fs-6 text-center">
-        <span>¿Ya tienes una cuenta?,
-        <router-link to="/Signin" class="text-center w-100">Inicia Sesión</router-link></span>
+        <span
+          >¿Ya tienes una cuenta?,
+          <router-link to="/Signin" class="text-center w-100"
+            >Inicia Sesión</router-link
+          ></span
+        >
       </span>
     </div>
   </div>
   <Footer />
-
 </template>
 
-
 <script>
-
 import Header from "../../components/Header.vue";
-import Footer from '../../components/Footer.vue';
-import { fetchSinToken } from "../../helpers/fetch";
+import Footer from "../../components/Footer.vue";
 import { customAlert } from "../../helpers/alerts";
 
-
 export default {
- name: "register",
+  name: "register",
   data() {
-    return { inputName: "", inputLastname: "", inputEmail: "", inputPassword: "", inputPasswordv:""};
+    return {
+      inputName: "",
+      inputLastname: "",
+      inputEmail: "",
+      inputPassword: "",
+      inputPasswordv: "",
+    };
   },
   components: {
     Header,
-    Footer
+    Footer,
   },
- 
+
   methods: {
-   
     register() {
       fetchSinToken(
         "api/v1/users/register",
-       {  "email": this.inputEmail,
-          "firstName": this.inputName,
-          "lastName": this.inputLastname,
-          "password": this.inputPassword,
-          "passwordConfirmation": this.inputPasswordv,
-         },
-         "POST",
-         {},
-         ).then(res =>{
+        {
+          email: this.inputEmail,
+          firstName: this.inputName,
+          lastName: this.inputLastname,
+          password: this.inputPassword,
+          passwordConfirmation: this.inputPasswordv,
+        },
+        "POST",
+        {}
+      )
+        .then((res) => {
           console.log(res);
           customAlert(
             "Registro Exitoso",
@@ -94,13 +128,11 @@ export default {
         });
     },
   },
- 
- 
+
   props: [],
   mounted() {},
-  
+
   computed: {},
-  
 };
 </script>
 
--- src/views/pages/Home.vue
+++ src/views/pages/Home.vue
@@ -1,55 +1,37 @@
 <template>
-<Header :logged="true"/>
- <div class="main-container">
-   <h1>Home</h1>
-   <!-- <Button
+  <Header :logged="true" />
+  <div class="main-container">
+    <h1>Home</h1>
+    <!-- <Button
               label="Nuevo Evento"
               icon="pi pi-plus"
               class="p-button-success mr-2"
               @click="$refs.eventModal.openNew()"
             /> -->
- </div>
+  </div>
 
- <!-- Modal ref -->
- <EventModal ref = "eventModal"/>
+  <!-- Modal ref -->
+  <EventModal ref="eventModal" />
 </template>
 
 <script>
-  import Header from '../../components/Header.vue';
-  import EventModal from '../../components/EventModal.vue';
-  import { fetchConToken } from '../../helpers/fetch';
-  export default {
-    name: 'Home',
-    
-    props: [],
-    mounted() {
-      
-      fetchConToken(
-        "api/v1/users/me",
-        {},
-        "GET"
-        
-      )
-      .then(res =>{
-         
-        sessionStorage.setItem('userData', JSON.stringify(res.data));
-    })
-    .catch(err =>{
-    console.log(err);
-    })
+import Header from "../../components/Header.vue";
+import EventModal from "../../components/EventModal.vue";
+export default {
+  name: "Home",
+
+  props: [],
+
+  data() {
+    return {};
   },
-    data() {
-      return {};
-    },
-    methods: {},
-    computed: {},
-    components: {
-      Header,
-      EventModal
-    }
-  };
+  methods: {},
+  computed: {},
+  components: {
+    Header,
+    EventModal,
+  },
+};
 </script>
 
-<style>
-
-</style>
+<style></style>
