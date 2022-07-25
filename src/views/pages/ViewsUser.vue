65744b46db6328ae5cd3b297bd34dd26c4ab9eb9
Edgardo <ramirezedgardo92@gmail.com>
2022-07-24 19:07:15 -0600

    to subscribe

--- src/views/pages/ViewsUser.vue
+++ src/views/pages/ViewsUser.vue
@@ -8,57 +8,33 @@
   <Button icon="pi pi-calendar" label="Eventos Privados" @click="eventprivate()" />
 
   <div class="main-container flex-container pt-2 px-4">
-    <Card
-      class="w-30"
-      v-for="eve in evento"
-      :key="eve._id"
-      style="padding: 1rem; margin-top: 15px"
-    >
+    <Card class="w-30" v-for="eve in evento" :key="eve._id" style="padding: 1rem; margin-top: 15px">
       <template #header>
-        <img
-          src="https://umad.edu.mx/wp-content/uploads/2018/09/ingenieri%CC%81a-de-software.jpg"
-          style="height: 150px"
-          icon="pi pi-pencil"
-        />
+        <img src="https://umad.edu.mx/wp-content/uploads/2018/09/ingenieri%CC%81a-de-software.jpg" style="height: 150px"
+          icon="pi pi-pencil" />
       </template>
       <template #title> Evento: {{ eve.title }} </template>
       <template #content> Ponente: {{ eve.speaker }} </template>
       <template #footer>
-        <Button icon="pi pi-check" label="Suscribirse" />
+        <Button label="Suscribirse" icon="pi pi-pencil" class="p-button-text" @click="subscribe(eve._id)" />
       </template>
     </Card>
   </div>
 
-  <Dialog
-    v-model:visible="eventoDialog"
-    :style="{ width: '800px', align: center }"
-    header="Eventos Privados"
-    :modal="true"
-  >
+  <Dialog v-model:visible="eventoDialog" :style="{ width: '800px', align: center }" header="Eventos Privados"
+    :modal="true">
     <div class="p-flu">
-      <Card
-        style="width: 45%; padding: 1rem; margin: 1rem"
-        v-for="privateEve in eventoprivado"
-        :key="privateEve._id"
-      >
+      <Card style="width: 45%; padding: 1rem; margin: 1rem" v-for="privateEve in eventoprivado" :key="privateEve._id">
         <template #header>
-          <img
-            src="https://umad.edu.mx/wp-content/uploads/2018/09/ingenieri%CC%81a-de-software.jpg"
-            style="height: 150px"
-            icon="pi pi-pencil"
-          />
+          <img src="https://umad.edu.mx/wp-content/uploads/2018/09/ingenieri%CC%81a-de-software.jpg"
+            style="height: 150px" icon="pi pi-pencil" />
         </template>
         <template #title class="content"> Evento: {{ privateEve.title }} </template>
 
         <template #subtitle>Ponente: {{ privateEve.speaker }} </template>
 
         <template #footer>
-          <Button
-            label="Suscribirse"
-            icon="pi pi-pencil"
-            class="p-button-text"
-            @click="update(privateEve._id)"
-          />
+          <Button label="Suscribirse" icon="pi pi-pencil" class="p-button-text" @click="subscribe(privateEve._id)" />
         </template>
       </Card>
     </div>
@@ -101,6 +77,25 @@ export default {
           );
         });
     },
+
+    async subscribe(_id) {
+      if (confirm("Esta seguro de suscribirse a este evento??")) {
+        const res = await fetchConToken(`api/v1/events/subscribed/${_id}`, {}, "POST")
+          .then((res) => {
+            console.log("Se sucribio al evento");
+            console.log(_id)
+          })
+          .catch(() => {
+            alert(
+              "Ha ocurrido un error",
+              "Ocurrio un error al suscribirse al evento  el evento.",
+              "error"
+            );
+          });
+      }
+    }
+
+
   },
 
   data() {
