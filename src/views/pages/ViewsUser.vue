68f860c6fb015a51b2558c13910eba5d8accdd50
Edgardo <ramirezedgardo92@gmail.com>
2022-08-06 10:50:57 -0600

    button changes

--- src/views/pages/ViewsUser.vue
+++ src/views/pages/ViewsUser.vue
@@ -3,41 +3,20 @@
 
   <div class="main-container flex-container pt-2 px-4">
     <div class="controls w-100 m-0 p-0">
-      <Button
-        icon="pi pi-calendar"
-        label="Subscripciones"
-        @click="$refs.EventMe.getEventMe()"
-      />
       <div class="d-inline-flex p-2 simple-list-example-scrollspy text-center">
         <a class="list-group-item list-group-item-action evpriv p-3 mb-2" href="#private">
-          <i class="pi pi-lock"></i> Eventos Privados</a
-        >
+          <i class="pi pi-lock"></i> Eventos Privados</a>
       </div>
     </div>
 
-    <Card
-      class="w-30"
-      v-for="eve in evento"
-      :key="eve._id"
-      style="padding: 1rem; margin-top: 15px"
-    >
+    <Card class="w-30" v-for="eve in evento" :key="eve._id" style="padding: 1rem; margin-top: 15px">
       <template #header>
-        <img
-          :src="eve.image?.secureUrl"
-          alt="no funciono"
-          style="height: 150px"
-          icon="pi pi-pencil"
-        />
+        <img :src="eve.image?.secureUrl" alt="no funciono" style="height: 150px" icon="pi pi-pencil" />
       </template>
       <template #title> Evento: {{ eve.title }} </template>
       <template #content> Ponente: {{ eve.speaker }} </template>
       <template #footer>
-        <Button
-          label="Suscribirse"
-          icon="pi pi-pencil"
-          class="p-button-text"
-          @click="subscribe(eve._id)"
-        />
+        <Button label="Suscribirse" icon="pi pi-pencil" class="p-button-text" @click="subscribe(eve._id)" />
       </template>
     </Card>
   </div>
@@ -46,46 +25,28 @@
     <p class="text-center text-muted fs-3"><a id="private">Eventos Privados</a></p>
   </div>
   <div class="main-container flex-container pt-2 px-4">
-    <Card
-      class="w-30"
-      style="padding: 1rem; margin: 1rem"
-      v-for="privateEve in eventoprivado"
-      :key="privateEve._id"
-    >
+    <Card class="w-30" style="padding: 1rem; margin: 1rem" v-for="privateEve in eventoprivado" :key="privateEve._id">
       <template #header>
-        <img
-          :src="privateEve.image?.secureUrl"
-          alt="Image"
-          style="height: 150px"
-          icon="pi pi-pencil"
-        />
+        <img :src="privateEve.image?.secureUrl" alt="Image" style="height: 150px" icon="pi pi-pencil" />
       </template>
       <template #title class="content"> Evento: {{ privateEve.title }} </template>
       <template #subtitle>Ponente: {{ privateEve.speaker }} </template>
       <template #footer>
-        <Button
-          label="Suscribirse"
-          icon="pi pi-pencil"
-          class="p-button-text"
-          @click="subscribe(privateEve._id)"
-        />
+        <Button label="Suscribirse" icon="pi pi-pencil" class="p-button-text" @click="subscribe(privateEve._id)" />
       </template>
     </Card>
   </div>
-
-  <EventMe ref="EventMe" />
+  <div class="container">holaholaholaholaholaholaholaholaholaholaholaholaholaholaholaholaholaholaholaholahola</div>
 </template>
 
 <script>
 import Header from "../../components/Header.vue";
 import { customAlert } from "../../helpers/alerts.js";
 import { fetchConToken } from "../../helpers/fetch.js";
-import EventMe from "../../components/EventMe.vue";
 export default {
   name: "ViewsUser",
   components: {
     Header,
-    EventMe,
   },
 
   methods: {
@@ -157,6 +118,7 @@ export default {
   color: orange;
   font-weight: bold;
 }
+
 .evpriv:hover {
   font-size: larger;
   color: darkslateblue;
